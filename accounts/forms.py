936149e271347ae502ffa35e4c7543d74670d60f
Developer-felix <onjombafelix1@gmail.com>
2021-07-02 00:54:40 -0700

    Added user member form

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import ChoiceField
+
+
+class AddStudentForm(forms.Form):
+    email=forms.EmailField(label="Email",max_length=50,widget=forms.EmailInput(attrs={"class":"form-control","autocomplete":"off"}))
+    password=forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name=forms.CharField(label="First Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name=forms.CharField(label="Last Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    username=forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control","autocomplete":"off"}))
